1c8be98b3b93d8472bb5cc69728621cf1e7a83e5
Sean T. Allen <sean@seantallen.com>
2023-12-09 23:23:19 +0000

    Fix truncation of good first issues when posting to Zulip
    The Zulip API has a max message size and our "good first issues"
    posting hits it resulting in message truncation and a loss of
    content.
    
    This commit adds a Python script to batch up "good first issues"
    content into 1 or more messages so that it doesn't get truncated.
    
    Also includes new image to run in as we need additional dependencies
    that the "post sync" image doesn't need.
    
    And this adds a PR check to link the python script and make sure it
    is all kosher.
    
    Fixes #18

---
+++ post-good-first-issues.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# pylint: disable=C0103
+# pylint: disable=C0114
+
+from datetime import datetime
+import sys
+import zulip
+
+messages = []
+current_message = ""
+for line in sys.stdin.readlines():
+    if len(current_message) + len(line) <= 7000:
+        current_message += line + "\n"
+    else:
+        # We have a decent chunk of cotent to post at this point.
+        # We are now looking for the next repository to start a new message.
+        # We don't want to break during a repository as it messes up the
+        # markdown formatting of the message in Zulip.
+        if line.startswith("**ponylang/"):
+            messages.append(current_message)
+            current_message = line + "\n"
+        else:
+            current_message += line + "\n"
+
+# add the final message in to messages
+messages.append(current_message)
+
+# set up topic with today's date
+msg_date = datetime.today().strftime('%Y-%m-%d')
+msg_topic = "Good first issues as of " + msg_date
+
+zulip_client = zulip.Client()
+for message in messages:
+    request = {
+        "type": "stream",
+        "to": "contribute to Pony",
+        "topic": msg_topic,
+        "content": message
+    }
+    result = zulip_client.send_message(request)
